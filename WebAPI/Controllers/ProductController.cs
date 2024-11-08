1b17624e28e9eb1e46f422f347e0b1e5108b105f
TanPham2808 <xuantan.pham2808@gmail.com>
2024-11-08 18:37:09 +0700

    Thêm project FrontEnd

---
+++ WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
+using WebAPI.Entites;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly StoreContext _storeContext;
+        public ProductController(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+        }
+
+        [HttpGet("GetPro")]
+        public async Task<IActionResult> GetProduct()
+        {
+            var response = await _storeContext.Products.ToListAsync();
+            return Ok(response);
+        }
+
+        // GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductById(int id)
+        {
+            Product product = await _storeContext.Products.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(product);
+        }
+
+    }
+}
