6f06eb9c84274315b3fd77b9cafba71bc8672378
TanPham2808 <xuantan.pham2808@gmail.com>
2024-11-10 08:33:16 +0700

    Sử dụng interface cho TS

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,10 +1,8 @@
 import { useEffect, useState } from "react";
+import { Product } from "./product";
 
 function App() {
-  const [products, setProduct] = useState([
-    { name: "product1", price: 100.0 },
-    { name: "product2", price: 200.0 },
-  ]);
+  const [products, setProduct] = useState<Product[]>([]);
 
   useEffect(() => {
     fetch("http://localhost:5000/api/product")
@@ -16,8 +14,12 @@ function App() {
     setProduct((preState) => [
       ...preState,
       {
-        name: "product" + preState.length + 1,
-        price: (preState.length + 1) * 100,
+        id: preState.length + 101,
+        name: "product" + (preState.length + 1),
+        price: preState.length * 100 + 100,
+        brand: "some brean",
+        description: "some desctiption",
+        pictureUrl: "http://picsum.photo/200",
       },
     ]);
   };
@@ -26,10 +28,10 @@ function App() {
     <div>
       <h1>Re-Store</h1>
       <ul>
-        {products.map((item, index) => {
+        {products.map((product) => {
           return (
-            <li key={index}>
-              {item.name} - ${item.price}
+            <li key={product.id}>
+              {product.name} - ${product.price}
             </li>
           );
         })}
